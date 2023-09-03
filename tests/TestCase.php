cb7b27b6d64e9fcadcc4502bbc599dc0a503b9aa
ahmad kash <aaboyazen1996@gmail.com>
2023-09-03 16:45:20 +0300

    add EloquentCollection assertion to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,15 +4,19 @@ namespace Tests;
 
 use App\Models\User;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Mockery;
 use Mockery\MockInterface;
+use Tests\Traits\HasCollectionAssert;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use HasCollectionAssert;
 
     public function signIn(Authenticatable|MockInterface $user = null)
     {
---
+++ tests/Traits/HasCollectionAssert.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Traits;
+
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Collection;
+
+
+trait HasCollectionAssert
+{
+
+    public function assertEloquentCollectionsEqual(Collection|EloquentCollection $collection1, Collection|EloquentCollection $collection2, Collection|array $keys = []): self
+    {
+        $this->assertTrue($this->eloquentCollectionsAreEqual(...func_get_args()), "Failed to assert that collection1 is not equal to collection2");
+        return $this;
+    }
+
+    public function eloquentCollectionsAreEqual(Collection|EloquentCollection $collection1, Collection|EloquentCollection $collection2, Collection|array $keys = []): bool
+    {
+        $collection1 = $this->normalizeCollection($collection1, $keys);
+        $collection2 = $this->normalizeCollection($collection2, $keys);
+
+        return $collection1->diff($collection2)->isEmpty();
+    }
+
+    private function normalizeCollection(Collection|EloquentCollection $collection, Collection|array $keys = []): Collection
+    {
+        $collection = $collection->map(fn ($item) => collect($item->attributesToArray())->sortKeys());
+        if (collect($keys)->isEmpty())
+            return $collection;
+
+        return $collection->map(fn ($item) => $item->only($keys));
+    }
+}
